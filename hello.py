8983fc8e9f804e8f0155e2069c25c02652f68e00
Ivan <85578963+GitRetr0@users.noreply.github.com>
2022-06-12 00:15:02 +0300

    add new dialogue string

--- hello.py
+++ hello.py
@@ -2,3 +2,6 @@ name=input("What's yuor name?")
 Phrase="Hello"
 Greeting=Phrase+","+" "+name
 print(Greeting)
+question=input("How are you doing?\n")
+reply="Now I see you're "+question
+print(reply)
