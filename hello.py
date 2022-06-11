80a209649945671ba4ca3df4afc6796a5f2dbe07
Ivan <85578963+GitRetr0@users.noreply.github.com>
2022-06-12 01:15:43 +0300

    add bullshit

--- hello.py
+++ hello.py
@@ -5,3 +5,6 @@ print(Greeting)
 question=input("How are you doing?\n")
 reply="Now I see you're "+question
 print(reply)
+order=input("Would you like tea or coffee?\n")
+
+jndvjknvkAVjnAJcvnkjfnlNVJna
