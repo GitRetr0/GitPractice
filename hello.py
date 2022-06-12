a5b01329f79eacf5332f296f24a8b3dd2a7cdd9c
Ivan <85578963+GitRetr0@users.noreply.github.com>
2022-06-12 19:04:47 +0300

    add restaurant dialogue

--- hello.py
+++ hello.py
@@ -6,5 +6,19 @@ question=input("How are you doing?\n")
 reply="Now I see you're "+question
 print(reply)
 order=input("Would you like tea or coffee?\n")
-
-jndvjknvkAVjnAJcvnkjfnlNVJna
+#added restaurant dialog
+order=input("Would you like coffee or tea?\n")
+if order == "coffee" or order=="Coffee":
+    print("Coffee with milk?")
+elif order == "tea" or order=="Tea":
+    print("Tea with milk?")
+extended_order=input(" ")
+if extended_order =="yes":
+    print("Here is your " +order+" with milk")
+elif extended_order =="no":
+    print("Would you like to add sugar instead?")
+more_extended_order=input(" ")
+if more_extended_order == "yes":
+    print("Here we go, your " +order+ " with sugar!")
+elif more_extended_order == "no":
+    print("Then go fuck a brain to someone else!")
