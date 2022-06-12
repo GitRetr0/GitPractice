966ac9fb344ac809704b3dbe18e768545f6ea93d
Ivan <85578963+GitRetr0@users.noreply.github.com>
2022-06-12 10:01:32 +0300

    add the end of purchase (needs improvement)

--- hello.py
+++ hello.py
@@ -1,11 +1,10 @@
-name=input("What's yuor name?")
+name=input("What's yuor name?\n")
 Phrase="Hello"
 Greeting=Phrase+","+" "+name
 print(Greeting)
 question=input("How are you doing?\n")
 reply="Now I see you're "+question
 print(reply)
-
 #this is just a comment to be commited
 #some social interaction below
 order=input("Would you like to buy your wife some flowers or chocolate?\n")
@@ -13,3 +12,14 @@ if order=="flowers" or order == "Flowers":
     print("Roses or tulips?")
 elif order=="Chocolate" or "chocolate":
     print("Milk chocolate or dark?")
+choice=input("")
+    #now adding an end of purchase
+if choice == "roses"or "Roses":
+    print("Here is your flowers!")
+else:
+     print("Maybe sweets then?")
+neworder=input("")
+if neworder == "yes":
+    print("Milk chocolate or dark?")
+else:
+        print("Goodbye then")
